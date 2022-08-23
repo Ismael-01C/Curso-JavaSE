73605ccf14982369f6c21d181e0ea7beab12ea36
Pasante7 desa <p7@rgtdom.local>
2022-08-23 14:54:04 -0500

    Arreglos

---
+++ src/Arrays.java
@@ -0,0 +1,40 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+
+        String[][] cities = new String[4][2];
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Bannana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+        System.out.println();
+        System.out.println();
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Medellin";
+        cities[1][0] = "Colombia";
+        cities[1][1] = "Bogota";
+        cities[2][0] = "Mexico";
+        cities[2][1] = "Guadalajara";
+        cities[3][0] = "Mexico";
+        cities[3][1] = "CDMX";
+
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+
+    }
+}
